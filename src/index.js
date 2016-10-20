32bff4e3aa7e9afc08f2698ef215aa863b8e29ef
Sonny <sonny@beanloop.se>
2016-10-20 21:59:42 +0200

    some more redux stuff

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
 import App from './App';
 import './index.css';
 
-import { createStore } from 'redux'
 import synthApp from './lib/reducers'
+import { changeWaveForm, WaveForms } from './lib/actions'
+
 let store = createStore(synthApp)
 
-console.log(store.getState())
+let unsubscibe = store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(changeWaveForm(WaveForms.SAWTOOTH))
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
+
+unsubscibe()
