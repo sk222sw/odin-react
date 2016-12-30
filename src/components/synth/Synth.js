69e0df6db72fadef86614a954b514590a940b313
Sonny <sonny@beanloop.se>
2016-12-30 23:08:04 +0100

    computer keyboard HOC

--- src/components/synth/Synth.js
+++ src/components/synth/Synth.js
@@ -5,6 +5,7 @@ import React from 'react'
 import Waw from '../../lib/waw/waw'
 import { Waveforms } from '../waveforms/Waveforms'
 import { find } from '../../lib/helpers'
+import { KeyType } from '../../lib/types/types'
 
 type propTypes = {
   keys: any[],
@@ -18,68 +19,88 @@ type propTypes = {
   removeAllKeys: any,
 }
 
-class Synth extends React.Component {
-  constructor(props: propTypes) {
-    super()
+function ComputerKeyboardEnhancer(WrappedComponent) {
+  return class ComputerKeyboard extends WrappedComponent {
+    constructor(props: propTypes) {
+      super(props)
+      this.props = props
 
-    this.props = props
-    this.waw = new Waw()
-    this.waw.init()
-  }
+      this.waw = new Waw()
+      this.waw.init()
+    }
 
-  componentWillMount() {
-    global.window.addEventListener('keydown', this.handleKeyDown, false)
-    global.window.addEventListener('keyup', this.handleKeyUp, false)
-    global.window.addEventListener('blur', this.removeAllKeys, false)
-  }
+    componentWillMount() {
+      global.window.addEventListener('keydown', this.handleKeyDown, false)
+      global.window.addEventListener('keyup', this.handleKeyUp, false)
+      global.window.addEventListener('blur', this.removeAllKeys, false)
+    }
 
-  componentWillUnmount() {
-    global.window.removeEventListener('keydown', this.handleKeyDown, false)
-    global.window.removeEventListener('keyup', this.handleKeyUp, false)
-    global.window.removeEventListener('blur', this.removeAllKeys, false)
-  }
+    componentWillUnmount() {
+      global.window.removeEventListener('keydown', this.handleKeyDown, false)
+      global.window.removeEventListener('keyup', this.handleKeyUp, false)
+      global.window.removeEventListener('blur', this.removeAllKeys, false)
+    }
 
-  props: propTypes
-  waw: Waw
+    props: propTypes
+    waw: Waw
 
-  canFindKey = (key: string): boolean =>
-    find(this.props.pressedKeys, key)
+    keyIsPressed = (key: string): boolean =>
+      find(this.props.pressedKeys, key)
 
-  handleKeyDown = ({ key }: {key: string}) => {
-    if (this.canFindKey(key)) return
-    if (!this.props.keys.some(pressedKey => pressedKey.keyPress === key)) return
-    const { frequency } = this.props.keys.filter(k => k.keyPress === key)[0]
+    filterByKeyPress = (list: any[], key: string): KeyType =>
+      list.filter(k => k.keyPress === key)[0]
 
-    const oscillator = this.waw.createNewOscillator({
-      key,
-      frequency,
-      waveform: this.props.currentWaveform,
-    })
+    keyIsNotAssigned = (key: any): boolean =>
+      !this.props.keys.some(k => k.keyPress === key)
 
-    this.props.addKey(key)
-  }
+    handleKeyDown = ({ key }: {key: string}) => {
+      if (this.keyIsPressed(key) || this.keyIsNotAssigned(key)) return
 
+      const { frequency } = this.filterByKeyPress(this.props.keys, key)
 
-  handleKeyUp = ({ key }: {key: string}) => {
-    this.waw.deleteOscillator(key)
-    this.props.removeKey(key)
-  }
+      const oscillator = this.waw.createNewOscillator({
+        key,
+        frequency,
+        waveform: this.props.currentWaveform,
+      })
 
-  removeAllKeys = () =>
-    this.props.removeAllKeys()
+      this.props.addKey(key)
+    }
 
+    handleKeyUp = ({ key }: {key: string}) => {
+      this.waw.deleteOscillator(key)
+      this.props.removeKey(key)
+    }
 
-  render() {
-    return (
-      <div className="synth-container">
-        <Waveforms
-          waveforms={this.props.waveforms}
-          currentWaveform={this.props.currentWaveform}
-          onWaveformClick={this.props.onWaveformClick}
-        />
-      </div>
-    )
+    removeAllKeys = () => {
+      this.props.removeAllKeys()
+      this.waw.deleteAllOscillators()
+    }
+
+    render() {
+      return super.render()
+    }
   }
 }
 
-export default Synth
+class Synth extends React.Component {
+  constructor(props) {
+    super(props)
+    this.props = props
+  }
+
+  render() {
+    return (<div>
+      {this.props.pressedKeys.map(k => {
+        <div>k</div>
+      })}
+      <Waveforms
+        waveforms={this.props.waveforms}
+        currentWaveform={this.props.currentWaveform}
+        onWaveformClick={this.props.onWaveformClick}
+      />
+    </div>)
+  }
+}
+
+export default ComputerKeyboardEnhancer(Synth)
