943b069cb3fd0982af735df9df77397770c6395a
Sonny <sonny@beanloop.se>
2017-05-04 17:30:25 +0200

    remove nav

--- tests/layout_test.js
+++ tests/layout_test.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-import { shallow } from 'enzyme';
-import Nav from '../src/nav';
-import Layout from '../src/layout';
+import React from 'react'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import { shallow } from 'enzyme'
+import Layout from '../src/layout'
 
-const expect = chai.expect;
+const expect = chai.expect
 
-chai.use(chaiEnzyme());
+chai.use(chaiEnzyme())
 
 describe('<Layout />', () => {
 
-  it('renders main and Nav element', () => {
-    const wrapper = shallow(<Layout />);
 
-    expect(wrapper.find(Nav)).to.have.length(1);
-  });
-
-});
+})
