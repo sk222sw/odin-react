95e2b01f628b6c28ac05cf6738809689e21a547d
Sonny <sonny@beanloop.se>
2017-05-05 12:49:13 +0200

    curry actions

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,14 +1,16 @@
 // @flow
 
-import { createAction } from 'redux-actions'
 import * as C from './constants'
 
-export const changeKey = createAction(C.CHANGE_KEY, payload => payload)
-export const changeWaveform = createAction(C.CHANGE_WAVEFORM, payload => payload)
-export const addKey = createAction(C.ADD_KEY, payload => payload)
-export const removeKey = createAction(C.REMOVE_KEY, payload => payload)
-export const removeAllKeys = createAction(C.REMOVE_ALL_KEYS, payload => payload)
-export const changeAttack = createAction(C.CHANGE_ATTACK, payload => payload)
-export const changeDecay = createAction(C.CHANGE_DECAY, payload => payload)
-export const changeSustain = createAction(C.CHANGE_SUSTAIN, payload => payload)
-export const changeRelease = createAction(C.CHANGE_RELEASE, payload => payload)
+const action = type => payload => ({type, payload})
+
+export const changeKey =                action(C.CHANGE_KEY)
+export const changeWaveform =           action(C.CHANGE_WAVEFORM)
+export const addKey =                   action(C.ADD_KEY)
+export const removeKey =                action(C.REMOVE_KEY)
+export const removeAllKeys =            action(C.REMOVE_ALL_KEYS)
+export const changeAttack =             action(C.CHANGE_ATTACK)
+export const changeDecay =              action(C.CHANGE_DECAY)
+export const changeSustain =            action(C.CHANGE_SUSTAIN)
+export const changeRelease =            action(C.CHANGE_RELEASE)
+export const changeOscillatorWaveform = action(C.CHANGE_OSCILLATOR_WAVEFORM)
